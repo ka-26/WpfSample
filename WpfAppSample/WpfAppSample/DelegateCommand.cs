2ee07523469361b2cdda58898c0bf5226b74ada3
ka-26 <ka.wa.26mr@gmail.com>
2021-10-13 02:18:05 +0900

    サンプル追加

---
+++ WpfAppSample/WpfAppSample/DelegateCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppSample
+{
+    internal class DelegateCommand : ICommand
+    {
+        private Action<object> _Execute;
+        private Func<object, bool> _CanExecute;
+
+        public DelegateCommand(Action<object> Execute)
+          : this(Execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        {
+            this._Execute = Execute;
+            this._CanExecute = CanExecute;
+        }
+
+        #region ICommandの実装
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+          => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public bool CanExecute(object parameter)
+          => (this._CanExecute != null) ? this._CanExecute(parameter) : true;
+
+        public void Execute(object parameter)
+        {
+            if (this._Execute != null)
+            {
+                this._Execute(parameter);
+            }
+        }
+        #endregion
+    }
+}
---
+++ WpfAppSample/WpfAppSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Navigation;
+using System.Windows;
+
+namespace WpfAppSample.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region プロパティ・フィールド
+        /// <summary>ナビボタン1タイトル</summary>
+        string _naviButton1Content;
+        public string naviButton1Content
+        {
+            get => _naviButton1Content;
+            set
+            {
+                if (_naviButton1Content != value)
+                {
+                    _naviButton1Content = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>ナビボタン2タイトル</summary>
+        string _naviButton2Content;
+        public string naviButton2Content
+        {
+            get => _naviButton2Content;
+            set
+            {
+                if (_naviButton2Content != value)
+                {
+                    _naviButton2Content = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// コンストラクタ1
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            naviButton1Content = "ページ１";
+            naviButton2Content = "ページ２";
+        }
+
+        #endregion
+
+        #region INotifyPropertyChangedの実装
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+          => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+
+        #region デリゲートコマンド
+        private DelegateCommand _navigation;
+        public DelegateCommand navigation
+        {
+            get => this._navigation ?? (this._navigation = new DelegateCommand(
+              _ =>
+              {
+                  var navigationWindow = (NavigationWindow)Application.Current.MainWindow;
+                  navigationWindow.Navigate(new Views.Page2());
+              }));
+        }
+        #endregion
+    }
+}
