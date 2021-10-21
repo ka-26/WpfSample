c4b29aca26dbcbf6c2996c157711300655a5f9e4
s_kawatani <s_kawatani@softenergy-controls.co.jp>
2021-10-21 17:57:56 +0900

    列毎バインド成功

--- WpfAppSample/WpfAppSample/ViewModels/Page1ViewModel.cs
+++ WpfAppSample/WpfAppSample/ViewModels/Page1ViewModel.cs
@@ -11,57 +11,54 @@ namespace WpfAppSample.ViewModels
 {
     public class Page1ViewModel
     {
-        public ObservableCollection<CellDefinition> ColumnDefinition { get; set; } = new ObservableCollection<CellDefinition>();
+        public ObservableCollection<RowDefinition> rows { get; set; } = new ObservableCollection<RowDefinition>();
 
-        public ColDefinitions colDefinitions { get; set; } = new ColDefinitions();
+        public RowDefinition colDefinitions { get; set; } = new RowDefinition();
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
         public Page1ViewModel()
         {
-            ColumnDefinition.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Red  }, text = new BindingProperty<string> { Value = "充放電棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            ColumnDefinition.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Blue }, text = new BindingProperty<string> { Value = "180A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            ColumnDefinition.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Green }, text = new BindingProperty<string> { Value = "120A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            ColumnDefinition.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.White }, text = new BindingProperty<string> { Value = "休止棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            ColumnDefinition.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Gray }, text = new BindingProperty<string> { Value = "DCIR棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
+            rows.Add(new RowDefinition { columns = new ObservableCollection<ColDefinition>() });
+            rows.Add(new RowDefinition { columns = new ObservableCollection<ColDefinition>() });
+            rows.Add(new RowDefinition { columns = new ObservableCollection<ColDefinition>() });
+            rows.Add(new RowDefinition { columns = new ObservableCollection<ColDefinition>() });
+            rows.Add(new RowDefinition { columns = new ObservableCollection<ColDefinition>() });
 
-            colDefinitions.ColumnDefinition01.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Red }, text = new BindingProperty<string> { Value = "充放電棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition01.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Blue }, text = new BindingProperty<string> { Value = "180A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition01.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Green }, text = new BindingProperty<string> { Value = "120A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition01.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.White }, text = new BindingProperty<string> { Value = "休止棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition01.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Gray }, text = new BindingProperty<string> { Value = "DCIR棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
+            rows[0].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "充放電棚1\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Red } });
+            rows[0].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "充放電棚2\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Yellow } });
+            rows[0].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "充放電棚3\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Blue } });
 
-            colDefinitions.ColumnDefinition02.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Red }, text = new BindingProperty<string> { Value = "充放電棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition02.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Blue }, text = new BindingProperty<string> { Value = "180A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition02.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Green }, text = new BindingProperty<string> { Value = "120A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition02.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.White }, text = new BindingProperty<string> { Value = "休止棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition02.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Gray }, text = new BindingProperty<string> { Value = "DCIR棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
+            rows[1].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "180A棚1\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Magenta } });
+            rows[1].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "180A棚2\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.LightYellow } });
+            rows[1].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "180A棚3\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.LightBlue } });
 
-            colDefinitions.ColumnDefinition03.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Red }, text = new BindingProperty<string> { Value = "充放電棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition03.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Blue }, text = new BindingProperty<string> { Value = "180A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition03.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Green }, text = new BindingProperty<string> { Value = "120A棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition03.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.White }, text = new BindingProperty<string> { Value = "休止棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
-            colDefinitions.ColumnDefinition03.Add(new CellDefinition { backColor = new BindingProperty<Brush> { Value = Brushes.Gray }, text = new BindingProperty<string> { Value = "DCIR棚\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" } });
+            rows[2].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "120A棚1\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.White } });
+            rows[2].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "120A棚2\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Green } });
+            rows[2].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "120A棚3\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Brown } });
 
+            rows[3].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "休止棚1\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Gray } });
+            rows[3].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "休止棚2\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.LightCyan } });
+            rows[3].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "休止棚3\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Transparent } });
 
-
+            rows[4].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "DCIR棚1\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Red } });
+            rows[4].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "DCIR棚2\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Yellow } });
+            rows[4].columns.Add(new ColDefinition { text = new BindingProperty<string> { Value = "DCIR棚3\n---(4.3℃)\n待受(待機中)\nStep1/0(00:00:00)" }, backColor = new BindingProperty<Brush> { Value = Brushes.Blue } });
         }
     }
 
     /// <summary>
     /// datagrid 列定義
     /// </summary>
-    public class CellDefinition
+    public class RowDefinition
+    {
+        public ObservableCollection<ColDefinition> columns = new ObservableCollection<ColDefinition>();
+    }
+
+    public class ColDefinition
     {
         public BindingProperty<string> text { get; set; } = new BindingProperty<string>();
         public BindingProperty<Brush> backColor { get; set; } = new BindingProperty<Brush>();
-    }
-
-    public class ColDefinitions
-    {
-        public ObservableCollection<CellDefinition> ColumnDefinition01 { get; set; } = new ObservableCollection<CellDefinition>();
-        public ObservableCollection<CellDefinition> ColumnDefinition02 { get; set; } = new ObservableCollection<CellDefinition>();
-        public ObservableCollection<CellDefinition> ColumnDefinition03 { get; set; } = new ObservableCollection<CellDefinition>();
     }
 }
---
+++ WpfAppSample/WpfAppSample/Views/Converter/ChangeToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WpfAppSample.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace WpfAppSample.Views.Converter
+{
+    public class ChangeToColorConverter : IMultiValueConverter
+    {
+        object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            ViewModels.RowDefinition data;
+            DataGridCell cell;
+
+            try
+            {
+                data = (ViewModels.RowDefinition)values[0];
+                cell = (DataGridCell)values[1];
+            }
+            catch (InvalidCastException e)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return data.columns[cell.Column.DisplayIndex].backColor.Value;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
